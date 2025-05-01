5f2d635f3996ff3a105121b344e12ca0c42d4aac
WilliamVoster <46726271+WilliamVoster@users.noreply.github.com>
2025-05-01 13:32:49 +0200

    implemented symptom extraction, updated symptom schema in db, removed GraphDB remnants

--- api/api.py
+++ api/api.py
@@ -1,12 +1,15 @@
 
 
+import json
+from datetime import datetime, timezone
 
 import weaviate
 from weaviate.classes.config import Configure, Property, DataType, VectorDistances
 from weaviate.classes.query import MetadataQuery, Sort
+
 from fastapi import FastAPI, Query
 from fastapi import Request
-# from neo4j import GraphDatabase
+
 from langchain_ollama import OllamaEmbeddings, OllamaLLM
 from langchain_ollama.chat_models import ChatOllama
 from langchain.schema import HumanMessage, SystemMessage, AIMessage
@@ -27,9 +30,6 @@ client = weaviate.connect_to_custom(
 )
 
 
-# neo4j_driver = GraphDatabase.driver("bolt://neo4j:7687", auth=("neo4j", "password"))
-
-
 @app.get("/")
 async def root():
 
@@ -42,38 +42,16 @@ async def root():
 
     return collections
 
-    for col_name in collections:
-
-        # vector_db_schema += f"\n{collections[col_name]}"
-        vector_db_schema += f"\n{collections[col_name].properties}"
-        vector_db_schema += f"\n{collections[col_name].vectorizer}"
-        vector_db_schema += f"\n{collections[col_name].vectorizer_config}"
-
-    return_text.append(vector_db_schema)
-
-    return "\n".join(return_text)
-
-    # ollama_embedder = OllamaEmbeddings(
-    #     base_url="http://ollama:11434", 
-    #     model="llama3.2:latest"
-    # )
-    # prompt = "Please tell me a joke about the sun."
-    # response = ollama_embedder.embed_query(prompt)
-    # return {"message": f"Embedding:{response}"}
-
-# @app.get("/graph")
-# async def get_graph():
-#     with neo4j_driver.session() as session:
-#         result = session.run("MATCH (n) RETURN n LIMIT 1")
-#         return [record["n"] for record in result]
-
 
 async def process_llm_query(data, context = ""):
 
     history = []
 
+    if "history" not in data: 
+        data["history"] = []
+
     ai_role_message = \
-        """You are a medical assistant.
+        """You are a medical assistant, personal trainer and motivator!
         Use the provided information and context to answer accurately.
         Do not answer confidently if you are unsure about a question."""
     
@@ -122,74 +100,147 @@ def process_symptom_extraction(user_query: str):
         model="llama3.2:latest"
     )
 
-    response = model.invoke(
-        f"Extract only symptoms from the following text: \
-        {user_query}. \
-        Output only the symptoms as a comma-separated list, JSON format. \
-        No other words. No explanations. Example: ['symptom1', 'symptom2']"
-    )
+    llm_query = f"""You are extracting health and activity-related facts and symptoms from user prompts. 
+Only include what is explicitly or clearly implied.
+Output only the symptoms as a comma-separated list of objects, JSON format. 
+No other words. No explanations.
+For queries where no symptoms are present, please respond with the character: 0
 
-    return response
+An example of a prompt and accompanying desired response is provided:
+User prompt: "I use a wheelchair but I want to build upper body strength. I had a rotator cuff injury last year, 
+so I’m easing back into it. I feel really good about my current mobility situation now though, it was somewhat harder 
+right after the accident and I could only focus on the negatives." 
+Output: 
+[ 
+    {{ 
+        "symptom": "wheelchair user",
+        "symptom_confidence": "high",
+        "recency_specified": "not new",
+    }},
+    {{
+        "symptom": "rotator cuff injury",
+        "symptom_confidence": "high",
+        "recency_specified": "1 year ago"
+    }}
+]
+
+A second example is provided:
+User prompt: "Can you make me a work-out plan for this week, i want to work on chest and lower back?
+I went on a hike last week and stepped wrong, hurts a bit but I had a great time overall."
+Output:
+[
+    {{
+        "symptom": "sprained ankle",
+        "symptom_confidence": "low",
+        "recency_specified": "last week"
+    }},
+    {{
+        "symptom": "lower back issues",
+        "symptom_confidence": "low",
+        "recency_specified": "no data"
+    }}
+]
+
+A third example is provided:
+User prompt: "I fell down the ladder yesterday and my back hurts today. What football team should I go see next?
+Last week I came down with a fever, think I might have had a cold"
+Output:
+[
+    {{
+        "symptom": "back pain",
+        "symptom_confidence": "high",
+        "recency_specified": "yesterday"
+    }},
+    {{
+        "symptom": "the common cold",
+        "symptom_confidence": "medium",
+        "recency_specified": "last week"
+    }}
+]
+
+Now your turn, remember that no findings is an ok outcome (respond with 0), here is the new user prompt to be parsed:
+User prompt: "{user_query}" """
+    
+
+    response = model.invoke(llm_query)
+
+    try:
+        extracted_json = json.loads(response)
+
+        if "symptom" not in extracted_json[0]:
+            return "", False
+
+        return extracted_json, True
+
+    except Exception as e:
+        return e, False
 
 
+def db_create_object(payload: list, collection: str):
 
+    symptoms = client.collections.get(collection)
+
+    uuids = []
+    for symptom in payload:
+
+        iso_time_utc = datetime.now(timezone.utc).isoformat()
+  
+        symptom["date_recorded"] = iso_time_utc
+        symptom["location"] = [0, 0]
+
+        print(symptom)
+
+        uuid = symptoms.data.insert(symptom)
+
+        uuids.append(uuid)
+    
+    return uuids
+
+
+# Program version: 1 (MVP)
 @app.post("/api/query")
 async def query(request: Request):
 
     data = await request.json()
-
-    extracted = process_symptom_extraction(data["query"])
-    return extracted
     
     return await process_llm_query(data)
 
 
+# Program version: 2 (with context)
 @app.post("/api/query_with_context")
 async def query_with_context(request: Request):
 
     data = await request.json()
 
-    search_term = data["query"]
+    user_query = data["query"]
 
+    # Context fetching
+    db_response = await process_db_query(user_query, "Symptoms")
+    
+    # Context extraction
+    extracted, save_symptoms = process_symptom_extraction(user_query)
 
-    db_response = await process_db_query(search_term, "Symptoms")
+    print(extracted)
+    if save_symptoms:
+        uuids = db_create_object(extracted, collection="Symptoms")
 
+    # Context builder
     context_builder = []
     for o in db_response.objects:
-
-        context_builder.append(f"{o.properties['entity']} is {o.properties['problem']}")
-    
+        context_builder.append(
+            f"Recorded {o.properties['symptom']}, \
+            with {o.properties['symptom_confidence']} confidence, \
+            at a time of {o.properties['recency_specified']} - \
+            recorded at: {o.properties['date_recorded']}"
+        )
     context = "\n".join(context_builder)
 
-    # data["query"] = f"Context: \n{context} \n\n Query: {search_term}"
 
     return await process_llm_query(data, context)
 
 
-
-@app.get("/create_object")
-async def create_object():
-
-    symptoms = client.collections.get("Symptoms")
-
-    uuid = symptoms.data.insert({
-        "entity": "right ankle",
-        "problem": "broken",
-        "location": [7.0, 15.0]
-    })
-
-    print("object's id: ", uuid)
-    return {"message": f"object's id: {uuid}"}
-
-
-@app.get("/create_collection")
+@app.get("/api/create_collection")
 async def create_collection():
-
-    #     generative_config = Configure.Generative.ollama(            # generative integration
-    #         api_endpoint="http://host.docker.internal:11434",       # Allow Weaviate from within a Docker container to contact your Ollama instance
-    #         model="llama3.2",
-    #     )
-    # )
 
     client.collections.delete("Symptoms")
     symptoms = client.collections.create(
@@ -203,10 +254,12 @@ async def create_collection():
         ),
         # reranker_config=Configure.Reranker.cohere(),                    # Reranker improves ordering of results. SENDS ONLINE API CALLS! 
         properties=[
-            Property(name="entity", data_type=DataType.TEXT),
-            Property(name="problem", data_type=DataType.TEXT),
+            Property(name="symptom", data_type=DataType.TEXT),
+            Property(name="symptom_confidence", data_type=DataType.TEXT),
             Property(name="location", data_type=DataType.NUMBER_ARRAY),
-            Property(name="date", data_type=DataType.DATE),
+            Property(name="date_recorded", data_type=DataType.DATE),
+            Property(name="recency_specified", data_type=DataType.TEXT),
+            Property(name="full_user_text", data_type=DataType.TEXT),
         ]
     )
 
@@ -214,17 +267,34 @@ async def create_collection():
     return {"message": f"created collection: {symptoms}"}
 
 
-@app.post("/api/create_object")
-async def create_object(request: Request):
+@app.get("/api/create_object")
+async def create_object():
 
-    data = await request.json()
+    symptoms = client.collections.get("Symptoms")
 
-    collection = client.collections.get(data["Collection"])
-
-    uuid = collection.data.insert(data["Object"])
+    uuid = symptoms.data.insert({
+        "symptom": "broken right ankle",
+        "symptom_confidence": "high",
+        "location": [7.0, 15.0],
+        "date_recorded": "2025-05-01T12:08:00+00:00",
+        "recency_specified": "not sure"
+    })
 
     print("object's id: ", uuid)
-    return {"message": f"object's id: {uuid}"}
+    return {"message": f"created object with id: {uuid}"}
+
+
+# @app.post("/api/create_object")
+# async def create_object_with_payload(request: Request):
+
+#     data = await request.json()
+
+#     payload = data["payload"]
+
+#     uuids = db_create_object(payload=payload, collection=data["collection"])
+
+#     return f"created object(s) with id(s): {uuids}"
+
 
 
 @app.post("/api/delete_object")
@@ -249,9 +319,7 @@ async def delete_object(request: Request):
             "status_code": e.status_code}
         
 
-    
-
-@app.get("/get_all")
+@app.get("/api/get_all")
 async def get_all():
 
     symptoms = client.collections.get("Symptoms")
@@ -271,7 +339,7 @@ async def process_db_query(term: str, collection_name: str):
         query=term,
         limit=10,
         return_metadata=MetadataQuery(distance=True, certainty=True, creation_time=True, last_update_time=True),
-        return_properties=["entity", "problem", "date", "location"],
+        return_properties=["symptom", "symptom_confidence", "date_recorded", "location", "recency_specified"],
         include_vector=True
     )
 
@@ -291,7 +359,7 @@ async def process_db_query(term: str, collection_name: str):
     return response
 
 
-@app.get("/get_near")
+@app.get("/api/get_near")
 async def get_near(term: str = Query(..., description="Search term for Weaviate")):
 
     response = await process_db_query(term, "Symptoms")
