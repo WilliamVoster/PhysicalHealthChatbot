6d93429310ce365e5d9cc22246bc0a74731633a0
WilliamVoster <46726271+WilliamVoster@users.noreply.github.com>
2025-05-24 14:24:01 +0200

    added new agentified system using langgraph and tools

--- api/api.py
+++ api/api.py
@@ -12,7 +12,16 @@ from fastapi import Request
 
 from langchain_ollama import OllamaEmbeddings, OllamaLLM
 from langchain_ollama.chat_models import ChatOllama
-from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, ToolMessage
+from langchain_core.runnables import RunnableLambda
+
+from langchain.tools import Tool
+from langgraph.graph import StateGraph, START, END, Graph, MessagesState
+
+import asyncio
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env")
 
 app = FastAPI()
 
@@ -28,6 +37,11 @@ client = weaviate.connect_to_custom(
     # }
 )
 
+llm = ChatOllama(
+    base_url="http://ollama:11434", 
+    model="llama3.2:latest",
+    temperature=0
+)
 
 @app.get("/")
 async def root():
@@ -42,24 +56,26 @@ async def root():
     return collections
 
 
-async def process_llm_query(data, user_context = "", article_context = ""):
+async def process_llm_query(data, user_context = "", article_context = "", user_activity_quotient = None):
 
     history = []
 
     if "history" not in data: 
         data["history"] = []
 
+    # chat = ChatOllama(
+    #     base_url="http://ollama:11434", 
+    #     model="llama3.2:latest"
+    # )
+
     ai_role_message = \
-        """You are a medical assistant, personal trainer and motivator!
+        """You are an expert on physcial health, a personal trainer and motivator!
+        You are not a trained medical professional.
         Use the provided information and context to answer accurately.
-        Do not answer confidently if you are unsure about a question."""
+        Do not answer confidently if you are unsure about a question.
+        Please answer in one paragraph or less, where possible."""
     
     history.append(SystemMessage(content=ai_role_message))
-
-    chat = ChatOllama(
-        base_url="http://ollama:11434", 
-        model="llama3.2:latest"
-    )
 
     for role, content in data["history"]:
         print(role)
@@ -74,14 +90,21 @@ async def process_llm_query(data, user_context = "", article_context = ""):
         elif role == "SYSTEM":
             history.append(SystemMessage(content=content))
 
+    if isinstance(user_activity_quotient, float):
+        content = \
+            f"An automated system silently found their current AQ score: {user_activity_quotient}. \n" \
+            f"The user did not mention this themselves." \
+            f"AQ stands for activity quotient, and is the successor to the PAI score (personal activity intelligence)." \
+            f"It scores a persons physcial activity, where 100 points gives the maximum possible health benefits."
+
+        history.append(SystemMessage(content=content))
+        data["history"].append(["SYSTEM", content])
+
     if len(user_context) > 0:
-        history.append(SystemMessage(
-            content=f"Context about the user: {user_context}")
-        )
-        data["history"].append([
-            "SYSTEM", 
-            f"Context about the user: {user_context}"
-        ])
+        content = f"Context about the user: {user_context}"
+
+        history.append(SystemMessage(content=content))
+        data["history"].append(["SYSTEM", content])
 
     if len(article_context) > 0:
         content = \
@@ -90,7 +113,6 @@ async def process_llm_query(data, user_context = "", article_context = ""):
             f"it is also fine to not use these in your response: {article_context}"
         
         history.append(SystemMessage(content=content))
-
         data["history"].append(["SYSTEM", content])
 
     prompt = data["query"]
@@ -100,7 +122,7 @@ async def process_llm_query(data, user_context = "", article_context = ""):
 
     print("HISTORY", history)
 
-    response = chat(history)
+    response = llm(history)
 
     data["history"].append(["AI", response.content])
 
@@ -278,6 +300,8 @@ async def process_db_query_symptoms(term: str):
 
     # Use certainty as confidence to display to user
 
+    print("ÆÆÆÆÆÆÆÆ process_db_query_symptoms ", response)
+
     return response
 
 
@@ -302,7 +326,189 @@ async def process_db_query_articles(term: str):
         include_vector=False
     )
 
+    print("ØØØØØ process_db_query_articles ", response)
     return response
+
+
+def get_activity_quotient(term: str) -> float:
+    aq = 12.0
+    return f"The user's current activity quotient (AQ) is {aq}."
+
+
+async def retrieve_user_symptoms(term: str) -> str:
+    return_object = await process_db_query_symptoms(term)
+    context = build_user_context(return_object)
+    return context
+
+async def retrieve_articles(term: str) -> str:
+    
+    return_object = await process_db_query_articles(term)
+    context = build_article_context(return_object)
+    return context
+
+tool_retrieve_user_attributes = Tool(
+    name="retrieve_user_attributes",
+    func=retrieve_user_symptoms,
+    description="Fetch attributes and symptoms about the user to use for additional context."
+)
+
+tool_retrieve_articles = Tool(
+    name="retrieve_articles",
+    func=retrieve_articles,
+    description="Fetch chunks of text from published articles on physical health to use for additional context."
+)
+
+tool_retrieve_user_activity_quotient = Tool(
+    name="retrieve_user_activity_quotient",
+    func= get_activity_quotient,
+    description="Fetch the user's current AQ / Activity Quotient. This is a score from 0-100 of how physically active they are."
+)
+
+tool_no_tool = Tool(
+    name="no_tool", 
+    func=lambda x : x, 
+    description="Move to answer the user's query. You have gathered all the context you need for this prompt."
+)
+
+
+def node_router(state: MessagesState):
+    
+    messages = [SystemMessage(content=
+        "You are an assistant with access to tools. "
+        "Only call a tool if it's necessary to answer the user's question. "
+        # "If multiple tools seem relevant, call them one at a time in separate turns, based on available context. "
+        # "e.g. you might need the output of one to query the other. "
+        # "You must always use at least one too, even though you might want to answer directly - instead call the 'no_tool' tool"
+        "If it's a simple / general question not related to physical health, just respond directly."
+    )] + state["messages"]
+
+    print("\n\n ********************Router MESSAGES: \t", messages, "\n\n")
+
+    response = llm\
+        .bind_tools([
+            tool_no_tool, 
+            tool_retrieve_user_attributes, 
+            tool_retrieve_articles, 
+            tool_retrieve_user_activity_quotient
+        ]).invoke(messages)
+    
+    print("\n\n *********************Router RESPONSE: \t", response, "\n\n")
+    
+    return {"messages": state["messages"] + [response]}
+
+def node_generate_answer(state: MessagesState):
+    print("\n\ngenerate_answer ---> ", state, "\n\n")
+
+    question = state["messages"][0].content
+    context = ""
+
+    for msg in state["messages"]:
+        if type(msg) == ToolMessage:
+            if msg.name != "no_tool":
+                context = f"[{msg.content}] from {msg.name}, the next context provided is:{context}"
+
+    system_prompt = \
+        f"You are an assistant. "\
+        f"You may use the following pieces of retrieved context to answer the user "\
+        f"If you don't know the answer, just say that you don't know. "\
+        f"Use three sentences maximum and keep the answer concise.\n"\
+        f"Context: [{context}]"
+    
+    print("PROMPT", system_prompt)
+    print("\n\n", question, "\nCCCCCCC\n")
+    
+    response = llm.invoke([
+        {"role": "system", "content": system_prompt}, 
+        {"role": "user", "content": question}
+    ])
+
+    return {"messages": [response]}
+
+def router_select_tool(state: MessagesState):
+
+    print("BBBBBBSSSSSSSSSSSSSSSSS router select tool state", state, "\n\n")
+
+    msg = state["messages"][-1]
+    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", type(msg), msg)
+
+    tool_calls = msg.tool_calls
+
+    print(type(tool_calls))
+    print(len(tool_calls))
+
+
+    if len(tool_calls) > 0:
+
+        tool_list = []
+        for tool in tool_calls:
+            tool_list.append(tool["name"])
+
+        print(tool_list)
+
+        return tool_list
+    
+    # print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX_____", type(a), a)
+
+    return "no_tool"
+
+def safe_tool_node_factory(tool):
+    async def node(state):
+        print("SAFE TOOOOL", state, "\n\n")
+        messages = state["messages"]
+        tool_messages = []
+
+        for msg in reversed(messages):
+
+            if isinstance(msg, AIMessage) and len(msg.tool_calls) > 0:
+
+                for call in msg.tool_calls:
+
+                    if call["name"] == tool.name:
+
+                        arg = call["args"].get("__arg1", "")
+
+                        if asyncio.iscoroutinefunction(tool.func):
+                            result = await tool.func(arg)
+                        else:
+                            result = tool.func(arg)
+
+                        tool_messages.append(ToolMessage(
+                            content=result,
+                            name=tool.name,
+                            tool_call_id=call["id"],
+                        ))
+                break
+
+        print("TOOOOL MESSAGESS:::", tool_messages)
+        return {"messages": messages + tool_messages}
+    return node
+
+
+workflow = StateGraph(MessagesState)
+workflow.add_node("router", node_router)
+workflow.add_node("retrieve_articles", safe_tool_node_factory(tool_retrieve_articles))
+workflow.add_node("retrieve_user_attributes", safe_tool_node_factory(tool_retrieve_user_attributes))
+workflow.add_node("retrieve_user_activity_quotient", safe_tool_node_factory(tool_retrieve_user_activity_quotient))
+workflow.add_node("generate_answer", node_generate_answer)
+
+workflow.add_edge(START, "router")
+workflow.add_conditional_edges(
+    "router",
+    router_select_tool,
+    {
+        "no_tool": "generate_answer", 
+        "retrieve_articles": "retrieve_articles", 
+        "retrieve_user_attributes": "retrieve_user_attributes",
+        "retrieve_user_activity_quotient": "retrieve_user_activity_quotient"
+    }
+)
+workflow.add_edge("retrieve_articles", "generate_answer")
+workflow.add_edge("retrieve_user_attributes", "generate_answer")
+workflow.add_edge("retrieve_user_activity_quotient", "generate_answer")
+workflow.add_edge("generate_answer", END)
+
+agent_graph = workflow.compile()
+
 
 
 # Program version: 1 (MVP)
@@ -310,21 +516,25 @@ async def process_db_query_articles(term: str):
 async def query(request: Request):
 
     data = await request.json()
+
+    aq = get_activity_quotient()
     
-    return await process_llm_query(data)
+    return await process_llm_query(data, user_activity_quotient=aq)
 
 
-# Program version: 2 (with user context)
+# Program version: 2 (with user + article context)
 @app.post("/api/query_with_context")
 async def query_with_context(request: Request):
 
     data = await request.json()
 
     user_query = data["query"]
+    aq = get_activity_quotient()
 
     # Context fetching
+    search_term = user_query + f"\n\nActivity quotient (AQ):{aq}"
     db_response_symptoms = await process_db_query_symptoms(user_query)
-    db_response_articles = await process_db_query_articles(user_query)
+    db_response_articles = await process_db_query_articles(search_term)
 
     # Context building
     user_context = build_user_context(db_response_symptoms)
@@ -335,21 +545,40 @@ async def query_with_context(request: Request):
     if save_symptoms: uuids = db_create_symptom_object(extracted)
 
     
-    return await process_llm_query(data, user_context, article_context)
+    return await process_llm_query(data, user_context, article_context, user_activity_quotient=aq)
 
 
-# # Program version: 3 (with article context)
-# @app.post("/api/query_with_article_context")
-# async def query_with_article_context(request: Request):
+# Program version: 3 (agentified)
+@app.post("/api/query_agent")
+async def query_agent(request: Request):
 
-#     data = await request.json()
+    data = await request.json()
 
-#     user_query = data["query"]
+    user_query = data["query"]
 
-#     # Context fetching
-#     db_response = await process_db_query_articles(user_query)
 
-#     return db_response
+    print(
+        "\n##################################################################\n\n",
+        agent_graph.get_graph().draw_mermaid(), 
+        "\n\n##################################################################\n"
+    )
+
+    messages = await agent_graph.ainvoke({"messages": [{
+        "role": "user",
+        # "content": "hello", 
+        # "content": "what are possible symptoms of the drug 'Herbamed solice'?",
+        # "content": "am i working out enough?",
+        # "content": "do not fetch any expert knowledge. What is my name?",
+        # "content": "What is my name, and what is my AQ?",
+        # "content": "what is my Activity Quotient?",
+        "content": user_query,
+    }]})
+
+    final_response = messages["messages"][-1]
+    final_response.pretty_print()
+    
+    return final_response
+
 
 
 @app.get("/api/create_collection_symptoms")
@@ -421,19 +650,6 @@ async def create_object():
     return {"message": f"created object with id: {uuid}"}
 
 
-# @app.post("/api/create_object")
-# async def create_object_with_payload(request: Request):
-
-#     data = await request.json()
-
-#     payload = data["payload"]
-
-#     uuids = db_create_symptom_object(payload=payload)
-
-#     return f"created object(s) with id(s): {uuids}"
-
-
-
 @app.post("/api/delete_object")
 async def delete_object(request: Request):
 
@@ -483,7 +699,7 @@ async def get_all_chunks():
 @app.get("/api/get_symptomp_near")
 async def get_symptom_near(term: str = Query(..., description="Search-term for Weaviate")):
 
-    response = await process_db_query_symptoms(term, "Symptoms")
+    response = await process_db_query_symptoms(term)
 
     return {"response": response}
 
@@ -491,7 +707,7 @@ async def get_symptom_near(term: str = Query(..., description="Search-term for W
 @app.get("/api/get_article_near")
 async def get_article_near(term: str = Query(..., description="Search-term for Weaviate")):
 
-    response = await process_db_query_articles(term, "Articles_pubmed")
+    response = await process_db_query_articles(term)
 
     return {"response": response}
  
