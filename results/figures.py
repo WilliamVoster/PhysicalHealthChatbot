8dd695aa115b35165c7da5cdb373eee3864011be
WilliamVoster <46726271+WilliamVoster@users.noreply.github.com>
2025-06-30 16:18:10 +0200

    more figures, plotting compute times

---
+++ results/figures.py
@@ -0,0 +1,29 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+filename = "./Evaluation of generated prompts.csv"
+
+df = pd.read_csv(filename)
+
+print(df.head())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
